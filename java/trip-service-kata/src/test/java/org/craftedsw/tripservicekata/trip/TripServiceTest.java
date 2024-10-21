992fe52e742cea3bff6c7a517686f6774f226a75
Alexandre Duchenne <alexduch@hotmail.fr>
2024-10-21 17:04:28 +0200

    Should throw when user not logged in

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,25 @@
 package org.craftedsw.tripservicekata.trip;
 
-public class TripServiceTest {
-	
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserSession;
+import org.junit.jupiter.api.Test;
+
+class TripServiceTest {
+
+  private UserSession userSession = mock(UserSession.class);
+
+  private TripService tripService = new TripService(userSession);
+
+  @Test
+  void shouldThrowIfUserIsAnonymous() {
+    when(userSession.getLoggedUser()).thenReturn(null);
+
+    assertThrows(UserNotLoggedInException.class,
+        () -> tripService.getTripsByUser(new User()));
+  }
 }

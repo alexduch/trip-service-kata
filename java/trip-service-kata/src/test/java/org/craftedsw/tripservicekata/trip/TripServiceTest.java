a7258a3a0528214597dc5623de44d254c9caf237
Alexandre Duchenne <alexduch@hotmail.fr>
2024-10-21 17:17:53 +0200

    Should return an empty list if not a friend

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,9 +1,11 @@
 package org.craftedsw.tripservicekata.trip;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
@@ -21,5 +23,26 @@ class TripServiceTest {
 
     assertThrows(UserNotLoggedInException.class,
         () -> tripService.getTripsByUser(new User()));
+  }
+
+  @Test
+  void shouldReturnAnEmptyListIfTargetUserHasNoFriend() {
+    when(userSession.getLoggedUser()).thenReturn(new User());
+
+    List<Trip> trips = tripService.getTripsByUser(new User());
+
+    assertTrue(trips.isEmpty());
+  }
+
+  @Test
+  void shouldReturnAnEmptyListIfNotAFriend() {
+    when(userSession.getLoggedUser()).thenReturn(new User());
+    User targetUser = new User();
+    targetUser.addFriend(new User());
+    targetUser.addFriend(new User());
+
+    List<Trip> trips = tripService.getTripsByUser(targetUser);
+
+    assertTrue(trips.isEmpty());
   }
 }
